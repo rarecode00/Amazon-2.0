3fc42135271a378fff29ebb7753a338853399adc
Krishna Sharma <Ks9192174414@gmail.com>
2023-03-21 12:02:33 +0530

    modal created and database connectivity success

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,11 @@
+const connectToMongo = require('./database/connectivity')
 const express = require('express')
 const app = express();
 const port = 3000;
+connectToMongo();
+
+// app.use('/api/auth' , )
 
 app.listen(port , () =>{
     console.log("App listening on port: " , port);
-})
-
-app.get('/' , (req , res) =>{
-      res.send("Hello World")
 })
