86f16fd27af8fba6620e11ea1ccdadda0e1e6dfb
Krishna Sharma <Ks9192174414@gmail.com>
2023-03-30 23:15:45 +0530

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,25 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchData } from './Redux/Actions/actions';
+import React from 'react'
+import {createBrowserRouter , RouterProvider} from 'react-router-dom'
+import AddProduct from './components/AddProduct'
+import Main from './components/Main'
 
-function App() {
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.data);
-  const loading = useSelector(state => state.loading);
-  const error = useSelector(state => state.error);
+const Routers = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main/>,
 
-  useEffect(() => {
-    dispatch(fetchData('http://localhost:8080/api/product/fetch-products', 'GET'));
-  }, [dispatch]);
+    },{
+      path: '/add-product',
+      element: <AddProduct/>
+    }
+])
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
+const App = () => {
   return (
-    <div>
-      {console.log(data.products)}
-      successfully fetched
-      <img src = {'https://firebasestorage.googleapis.com/v0/b/ecommerce-3def5.appspot.com/o/Category-Mobile%2FRedmi-A1.jpg?alt=media&token=fe7f21b2-e3e4-4cbd-996f-72e6b8c58bde'}/>
-    </div>
-  );
+    <>
+     <RouterProvider router={Routers} />
+    </>
+  )
 }
 
-export default App;
+export default App
