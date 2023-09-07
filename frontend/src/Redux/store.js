fc8c254bcd1e0851de1dba85264b89d9f4cefb4a
Krishna Sharma <Ks9192174414@gmail.com>
2023-09-07 16:23:35 +0530

    Implement the new frontend with typescript

--- frontend/src/Redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import dataReducer from './Reducer/requestReducer';
-
-const store = createStore(dataReducer, applyMiddleware(thunk));
-
-export default store;
